ec12f2757886ce968e53292387e81cf7326d2529
tiarafbrnti <127468030+tiarafbrnti@users.noreply.github.com>
2023-11-24 11:04:13 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-    $koneksi = mysqli_connect("localhost", "root", "", "uts");
+    $koneksi = mysqli_connect("localhost", "root", "", "2106061_Tiara");
 
     if (!$koneksi) {
         echo"<script>alert('koneksi database gagal');</script>";
